ccc4dc1a3b2f0aebfaea00be10f52def34de2d48
Gilbert <ldes89150@gmail.com>
2021-03-26 21:53:58 -0400

    separate the struct

---
+++ pyDCAM/struct.py
@@ -0,0 +1,32 @@
+import ctypes
+
+class DCAMPROP_ATTR(ctypes.Structure):
+    """The dcam property attribute structure."""
+
+    _fields_ = [("cbSize", ctypes.c_int32),
+                ("iProp", ctypes.c_int32),
+                ("option", ctypes.c_int32),
+                ("iReserved1", ctypes.c_int32),
+                ("attribute", ctypes.c_int32),
+                ("iGroup", ctypes.c_int32),
+                ("iUnit", ctypes.c_int32),
+                ("attribute2", ctypes.c_int32),
+                ("valuemin", ctypes.c_double),
+                ("valuemax", ctypes.c_double),
+                ("valuestep", ctypes.c_double),
+                ("valuedefault", ctypes.c_double),
+                ("nMaxChannel", ctypes.c_int32),
+                ("iReserved3", ctypes.c_int32),
+                ("nMaxView", ctypes.c_int32),
+                ("iProp_NumberOfElement", ctypes.c_int32),
+                ("iProp_ArrayBase", ctypes.c_int32),
+                ("iPropStep_Element", ctypes.c_int32)]
+
+
+class DCAMPROP_VALUETEXT(ctypes.Structure):
+    """The dcam text property structure."""
+    _fields_ = [("cbSize", ctypes.c_int32),
+                ("iProp", ctypes.c_int32),
+                ("value", ctypes.c_double),
+                ("text", ctypes.c_char_p),
+                ("textbytes", ctypes.c_int32)]
