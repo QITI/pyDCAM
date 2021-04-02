c21db33e9567cd7b5171f98980361d32a2d2c5f5
Gilbert <ldes89150@gmail.com>
2021-04-02 16:53:16 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyDCAM',
+    version='',
+    packages=['pyDCAM'],
+    url='',
+    license='',
+    author='Chung-You Shih',
+    author_email='c5shih@uwaterloo.ca',
+    description=''
+)
